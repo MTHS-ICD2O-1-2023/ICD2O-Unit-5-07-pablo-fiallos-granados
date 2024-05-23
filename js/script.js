9c16b87840ff19fed08e2da78524c0bc97e7fe48
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-05-23 18:13:03 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked() {
+
+  // input
+
+  let n = parseInt(document.getElementById('n').value)
+
+  let sum = 0
+
+  // process
+
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+
+  // output
+
+  document.getElementById("user-info").innerHTML = sum
+}
